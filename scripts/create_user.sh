2a0454635f7202512f45274e9f5117546cd18dec
Andrew Nisbet <anisbet@epl.ca>
2017-12-14 17:12:08 -0700

    Still not working on the ssh call. Will have to solve some other way.

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -24,6 +24,7 @@
 # Author:  Andrew Nisbet, Edmonton Public Library
 # Copyright (c) Thu Feb 23 16:22:30 MST 2017
 # Rev: 
+#          0.3 - Fixing stuff so the script can run it. 
 #          0.2 - Dereference path variable fix. 
 #          0.1 - Dev. 
 #          0.0 - Dev. 
@@ -35,23 +36,22 @@
 ## The Node.js server will serialize data of users to be created into a file. 
 ## Find this script and convert to flat user (web service-usable format),
 ## and load via appropriate mechanism. See create_user.py for conversion functions.
-DATE_NOW=$(date +%Y%m%d)                       # Looks like: 20170728
+DATE_NOW=$(date +%Y%m%d%H%M%S)  # Looks like: 20171214164754
 WORK_DIR=/home/ilsadmin/create_user
 PY_CONVERTER=${WORK_DIR}/scripts/create_user.py
-JSON_USERS=${WORK_DIR}/incoming/user.data
-# FLAT_USERS=$WORK_DIR/incoming/user$DATE_NOW.$$.flat # Production setting.
-FLAT_USERS=${WORK_DIR}/incoming/user$DATE_NOW.flat      # Test setting.
+JSON_USERS=${WORK_DIR}/incoming/user.$$.data
+FLAT_USERS=${WORK_DIR}/incoming/user.$$.flat
 ERROR=${WORK_DIR}/create_user.log
 TEST_ILS="sirsi@edpl-t.library.ualberta.ca"  # Test server is default ILS to write to.
 PROD_ILS="sirsi@eplapp.library.ualberta.ca"  # Production server is default ILS to write to.
 SERVER="$TEST_ILS"                           # Default to test server for customer loading.
 LIBRARY="EPLMNA"
 TMP_DIR="/tmp"
-# SCRATCH=$TMP_DIR/create_user$$.out         # Production setting.
+# SCRATCH=${TMP_DIR}/create_user$DATE_NOW.out         # Production setting.
 SCRATCH=${WORK_DIR}/scripts/create_user.out    # Test setting.
 API_SWITCHES="-aU -bU -mc"     # Create user.
 USER="ADMIN|PCGUI-DISP"        # Load user for Symphony logging, site specific
-VERSION="0.2"
+VERSION="0.3"
 
 if  [ ! -s "$PY_CONVERTER" ]
 then
@@ -82,10 +82,10 @@ if [ -s "$1" ]; then
 		# Save customer ID for error log.
 		echo "loading users: " >>${ERROR}
 		# Record the ids of the customers that were loaded to the log.
-		grep USER_ID ${FLAT_USERS} | awk 'NF>1{print $NF}' | sed -e 's/^..//' >>${ERROR}
-		count_expected=$(grep USER_ID ${FLAT_USERS} | awk 'NF>1{print $NF}' | sed -e 's/^..//' | wc -l)
+		grep USER_ID ${FLAT_USERS} >>${ERROR}
+		count_expected=$(grep USER_ID ${FLAT_USERS} | wc -l)
 		# loadflatuser outputs the new user key to STDOUT if successful.
-		cat ${FLAT_USERS} | ssh ${SERVER} 'cat - | loadflatuser -aU -bU -l"ADMIN|PCGUI-DISP" -mc -n -y"EPLMNA" -d' >${SCRATCH}
+		cat ${FLAT_USERS} | ssh $SERVER 'cat - | loadflatuser -aU -bU -l"ADMIN|PCGUI-DISP" -mc -n -y"EPLMNA" -d' >${SCRATCH}
 		# For more sophisticated solutions see Metro loaduser.sh.
 		## Add a user.
 		# loadFlatUserCreate.add("loadflatuser");
@@ -109,10 +109,10 @@ if [ -s "$1" ]; then
 		echo "\$count_loaded='$count_loaded' compared with \$count_expected='$count_expected'"  >&2
 		if [ "$count_loaded" == "$count_expected" ]; then
 			echo "customer successfully loaded."
-			# rm ${SCRATCH}                  # Production setting.
+			rm ${SCRATCH}    # Production setting.
 			# Don't reload the customer data if it all worked out. However we need to make sure all the customers
 			# requested are loaded. The service can accept multiple customers on input.
-			# rm ${JSON_USERS} ${FLAT_USERS}   # Production setting.
+			# rm ${JSON_USERS} # ${FLAT_USERS}   # Production setting.
 			exit 0
 		else
 			echo "one or more customer load requests failed."
