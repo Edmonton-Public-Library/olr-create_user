f0d715ebf4a3356b566a024c720f48608cb8aa5e
beryltseng <56453474+beryltseng@users.noreply.github.com>
2019-10-25 10:35:45 -0600

    Replace fs.watcher with chokidar to avoid receiving duplicated events when a file is added to the folder

--- scripts/watcher.js
+++ scripts/watcher.js
@@ -3,24 +3,26 @@
 'use strict';
 
 const fs = require('fs');
+const chokidar = require('chokidar');
 const exec = require('child_process').exec;
+const path = require('path');
 
-const folder = process.argv[2] || `${process.env.HOME}/OnlineRegistration/olr-create_user/incoming`; // default if one is not specified
+const files = process.argv[2] || `${process.env.HOME}/OnlineRegistration/olr-create_user/incoming/*.data`; // default if one is not specified
 const runscript = process.argv[3] || `${process.env.HOME}/OnlineRegistration/olr-create_user/scripts/convert_user.sh`
 
 const usage = function() {
   console.log(`
     =============================================================================
-    node watcher <path to the folder to be monitored> <path to the script to run>
+    node watcher <path to the files to be monitored> <path to the script to run>
 
     if <path to the folder to be monitored> is not speified, it's default to
-    '/home/ilsadmin/OnlineRegistration/olr-create_user/incoming'
+    '/home/ilsadmin/OnlineRegistration/olr-create_user/incoming/*.data'
 
     if <path to the script to run> is not specified, it's default to
     '/home/ilsadmin/OnlineRegistration/olr-create_user/scripts/convert_user.sh'
 
     e.g.
-    node watcher $HOME/OnlineRegistration/olr-create_user/incoming $HOME/OnlineRegistration/olr-create_user/scripts/convert_user.sh
+    node watcher $HOME/OnlineRegistration/olr-create_user/incoming/*.data $HOME/OnlineRegistration/olr-create_user/scripts/convert_user.sh
   `);
 }
 
@@ -46,29 +48,33 @@ const validate = function(path) {
 
 const start = function() {
 
+  const folder = path.parse(files).dir;
+  console.log(`Folder to monitor: ${folder}`);
   validate(folder);
+
+  console.log(`Script to run: ${runscript}`);
   validate(runscript);
 
-  console.log(`Start monitoring folder: ${folder}`);
+  console.log(`Start monitoring files: ${files}`);
 
-  fs.watch(folder).on('change', (eventType, filename) => {
-  	console.log(`${new Date().toISOString()} On change: ${JSON.stringify(eventType)} - ${JSON.stringify(filename)}`);
-  	if (eventType === 'change' && filename.endsWith('.data')) {
-  		setImmediate(() => {
-  			exec(`${runscript}`, (err, stdout, stderr) => {
-          if (err) {
-            errorHandler(err, stderr);
-            // console.log(error);
-            // console.log(stderr);
-            // process.exit(1);
-          } else {
-            console.log(stdout);
-            console.log(stderr);
-          }
-  			}); // exec
-  		}); // setImmediate
-  	} // if
-  }); // onChange
+  // Use chokidar instead of fs to avoid receiving duplicated events
+  const watcher = chokidar.watch(`${files}`);
+  watcher
+    .on('add', (path) => {
+      console.log(`${new Date().toISOString()}: File ${path} has been added`);
+  		exec(`${runscript} ${path}`, (err, stdout, stderr) => {
+        if (err) {
+          // stop watching
+          watcher.close();
+          errorHandler(err, stderr);
+        } else {
+          console.log(stdout);
+          console.log(stderr);
+        }
+			}); // exec
+    }) // onAdd
+    .on('change', (path) => console.log(`${new Date().toISOString()}: File ${path} has been changed`))
+    .on('unlink', (path) => console.log(`${new Date().toISOString()}: File ${path} has been removed`));
 }
 
 start();
